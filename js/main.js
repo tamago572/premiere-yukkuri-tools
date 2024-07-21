ed7f36cf3a0acbdbd9aff9d18cc2cca3b8cd73b1
tamago572 <110117095+tamago572@users.noreply.github.com>
2024-07-21 16:59:04 +0900

    音声合成エリアを作ったゾ。

--- js/main.js
+++ js/main.js
@@ -5,18 +5,53 @@
     'use strict';
 
     var csInterface = new CSInterface();
-    
-    
+
     function init() {
-                
+
         themeManager.init();
-                
+
+        const dragArea = document.getElementById('dragArea');
+
+        const dragAreaText = document.getElementById('dragAreaText');
+
+        // ドラッグエリアにファイルが乗っているときの処理
+        dragArea.addEventListener('dragover', dragOver);
+        // ドラッグエリアにファイルが乗っているときの処理
+        dragArea.addEventListener('dragleave', dragleave);
+        // ドラッグエリアにファイルがドラッグされたときの処理
+        dragArea.addEventListener('drag', drag);
+
+
+        // ドラッグエリアにファイルが乗っているときの処理
+        function dragOver(e) {
+            e.preventDefault();
+            dragArea.classList.add('dragging');
+            dragAreaText.textContent = 'ドロップで字幕を挿入';
+        }
+
+        // ドラッグエリアからファイルが離れたときの処理
+        function dragleave(e) {
+            e.preventDefault();
+            dragArea.classList.remove('dragging');
+            dragAreaText.textContent = 'ここに音声ファイルをD&D';
+        }
+
+        // ドラッグエリアにファイルがドラッグされたときの処理
+        function drag(e) {
+            e.preventDefault();
+            dragArea.classList.add('dragging');
+            dragAreaText.textContent = 'ドロップで字幕を挿入';
+            csInterface.evalScript('sayHello()');
+
+        }
+
+
         $("#btn_test").click(function () {
             csInterface.evalScript('sayHello()');
         });
     }
-        
+
     init();
 
 }());
-    
+
