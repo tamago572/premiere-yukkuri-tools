ea4565903546cd49aae171fac18951b9a59120b8
tamago572 <110117095+tamago572@users.noreply.github.com>
2024-08-20 20:01:11 +0900

    テロップの挿入機能を実装。ただしプロパティ変更エラーが出る

--- jsx/hostscript.jsx
+++ jsx/hostscript.jsx
@@ -15,11 +15,6 @@ function insertAudioAndTitle(filePath, audioTrackNumber) {
         var currentCTI = getCurrentCTI();
         var activeSequence = app.project.activeSequence;
 
-        // オーディオトラックがあるか確認、無かったら作成
-        if (activeSequence.audioTracks.numTracks < 2) {
-            activeSequence.audioTracks.add();
-        }
-
         // オーディオトラックにクリップ (音声ファイル)を追加
         try {
             // クリップをシーケンスに配置 [1]はトラック番号 A1=0, A2=1...
@@ -27,6 +22,12 @@ function insertAudioAndTitle(filePath, audioTrackNumber) {
         } catch (e) {
             alert("音声ファイルの挿入に失敗しました: " + e.message);
         }
+
+
+        // ビデオトラックにMogrtを追加
+        var mogrtFilePath = "C:\\Users\\7f7fn\\AppData\\Roaming\\Adobe\\Common\\Motion Graphics Templates\\エクステンション用テスト.mogrt";
+        // var mogrtFilePath = "C:\\Users\\7f7fn\\Documents\\Premiere\\琴葉茜字幕.mogrt";
+        insertMogrtTitle(mogrtFilePath, 1);
     } else {
         alert("インポートしたアイテムが見つかりませんでした。");
     }
@@ -61,8 +62,7 @@ function importFilesToRoot(filePath) {
     var importedFilesResult = app.project.importFiles([filePath], true, app.project.rootItem, false);
 
     if (importedFilesResult) {
-        // TODO: デバッグ用。後で消す
-        alert("ファイルのインポートに成功しました。importedFilesの内容: " + importedFilesResult);
+        // alert("ファイルのインポートに成功しました。importedFilesの内容: " + importedFilesResult);
     } else {
         alert("ファイルのインポートに失敗しました。importedFilesの内容: " + importedFiles);
     }
@@ -75,8 +75,10 @@ function findImportedItem(filePath) {
 
     // ルートアイテムの子アイテムを探索
     for (var i = 0; i < projectItems.numItems; i++) {
+        // クリップまたはファイルの場合
         var item = projectItems[i];
         if (item.type === ProjectItemType.CLIP || item.type === ProjectItemType.FILE) {
+            // ファイルパスが一致する場合
             if (item.getMediaPath() === filePath) {
                 return item;
             }
@@ -85,6 +87,45 @@ function findImportedItem(filePath) {
     return null;
     
 }
+
+// シーケンスにMogrtを使用したテロップを挿入する関数
+function insertMogrtTitle(filePath, videoTrackNumber) {
+    // mogrtをシーケンスに配置
+    try {
+        var insertedMogrt = app.project.activeSequence.importMGT(filePath, getCurrentCTI().ticks, videoTrackNumber, 0);
+        
+        try {
+            // mogrtのプロパティを設定
+            var mogrtComponent = insertedMogrt.components[0];
+            mogrtComponent.properties[0].setValue("テロップの内容");
+
+        } catch (e) {
+            alert("Mogrtのプロパティ変更に失敗しました: " + e.message);
+
+        }
+    } catch (e) {
+        alert("Mogrtの挿入に失敗しました: " + e.message);
+    }
+    
+}
+
+
+// // クリップを特定する関数
+// function findInsertedClip(trackNumber, startTime) {
+//     var track = app.project.activeSequence.videoTracks[trackNumber];
+//     var clips = track.clips;
+
+//     for (var i = 0; i < clips.numItems; i++) {
+//         var clip = clips[i];
+//         if (clip.start.ticks === startTime) {
+//             return clip;
+//         }
+//     }
+
+//     alert("挿入したクリップが見つかりませんでした。");
+//     return null;
+// }
+
 
 // アラートを表示する関数 main.jsから呼び出す用
 function showAlert(msg) {
