c22ce6c4826357d1aace0e5830f65a4160b45eca
tamago572 <110117095+tamago572@users.noreply.github.com>
2024-08-27 23:01:22 +0900

    macOSの場合デフォルトでUTF-8になる機能を実装

--- js/init.js
+++ js/init.js
@@ -1,13 +1,18 @@
 const os = require('os');
 const platform = os.platform();
 
+const charactor_code = document.getElementById("charactor_code");
+let charactor_code_selectIndex = charactor_code.value;
 const nl_code = document.getElementById("nl_code");
 let nl_code_selectIndex = nl_code.value;
 
 if (platform === "win32") {
-    nl_code_selectIndex = 0;
+    charactor_code_selectIndex = 0; // Shift_JIS
+    nl_code_selectIndex = 0; // CR+LF
 } else {
-    nl_code_selectIndex = 1;
+    charactor_code_selectIndex = 1; // UTF-8
+    nl_code_selectIndex = 1; // LF
 }
 
 nl_code.selectedIndex = nl_code_selectIndex;
+charactor_code.selectedIndex = charactor_code_selectIndex;
